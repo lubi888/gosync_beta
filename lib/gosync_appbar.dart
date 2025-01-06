7964690abeead16b0e0daa00ff1b1e19c6dd1946
lubi888 <linuxubiquitous@gmail.com>
2025-01-06 14:47:41 +0000

    appbar hamburger yellow

--- lib/gosync_appbar.dart
+++ lib/gosync_appbar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_linkify/flutter_linkify.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:gosync_beta/gosync_text.dart';
@@ -10,6 +12,38 @@ import 'package:intl/intl.dart';
 // //   // the App.build method, and use it to set our appbar title.
 // //   title: Text(widget.title)
 const double kToolbarHeight = 155.0;
+var myMenuItems = <String>[
+  'Home',
+  'Profile',
+  'Setting',
+  'Privacy',
+  'Contact',
+  'Ethereum Website',
+];
+
+void onSelect(item) {
+  switch (item) {
+    case 'Home':
+      print('Home clicked for ethscyc homepage');
+      break;
+    // case 'Profile':
+    //   print('Profile clicked');
+    //   break;
+    // case 'Setting':
+    //   print('Playtime setting clicked');
+    //   break;
+    // case 'Privacy':
+    //   print('Privacy cliked');
+    //   break;
+    // case 'Contact':
+    //   print('Contact clicked');
+    //   break;
+    case 'Ethereum Website':
+      print('this is where we connect to the net ethereum webite');
+      //url launcher
+      break;
+  }
+}
 
 class GoSyncAppBar extends StatelessWidget implements PreferredSizeWidget {
   const GoSyncAppBar({super.key});
@@ -20,46 +54,29 @@ class GoSyncAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     () => Scaffold.of(context).openDrawer();
-    // TabController tabController = TabController(length: 3, vsync: this);
-    // TabController tabController;
-    // Scaffold.of(context).openDrawer();
-    // return const Placeholder();
-    // TabController tabController;
-
-    // const TabBarView(
-    //     // controller: tabController,
-    //     children: <Widget>[
-    //       GoSyncScrollbar0(),
-    //       GoSyncScrollbar1(),
-    //       GoSyncScrollbar2(),
-    //       // GoSyncAbout(),
-    //       // gosyncscrollbar2(),
-    //     ]);
-
     debugPrint("ethsync appbar loaded");
     print(TimeOfDay.now());
-
     // var vsync = 0;
     // TabController tabController = TabController(length: 3, vsync: this);
     return AppBar(
-      // title: const Text(goSyncTitle),
-      centerTitle: true,
-      // backgroundColor: Colors.amber,
-      // foregroundColor: Colors.red,
-      // elevation/: 30.0,
-      title: Text(
-        // goSyncTitle,
-        AppLocalizations.of(context)!.titledate,
-        textAlign: TextAlign.center,
-        style: const TextStyle(
-          // backgroundColor: Color.fromARGB(255, 207, 160, 17),
-          // backgroundColor: Colors.yellow,
-          fontStyle: FontStyle.italic,
-          fontSize: 24.0,
-          fontWeight: FontWeight.bold,
+        // title: const Text(goSyncTitle),
+        centerTitle: true,
+        // backgroundColor: Colors.amber,
+        // foregroundColor: Colors.red,
+        // elevation/: 30.0,
+        title: Text(
+          // goSyncTitle,
+          AppLocalizations.of(context)!.titledate,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            // backgroundColor: Color.fromARGB(255, 207, 160, 17),
+            // backgroundColor: Colors.yellow,
+            fontStyle: FontStyle.italic,
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+          ),
         ),
-      ),
-      bottom: TabBar(
+        bottom: TabBar(
           // controller: TabController(length: 3, vsync: vsync)
           // ScrollController scrollbarController = ScrollController();
           tabs: <Widget>[
@@ -90,29 +107,128 @@ class GoSyncAppBar extends StatelessWidget implements PreferredSizeWidget {
                   // install golang and geth
                 )),
             Tab(
-                icon: const Icon(
-                  Icons.android,
-                  // color: Colors.greenAccent
-                ),
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Text(AppLocalizations.of(context)!.danger),
-                )),
-            // Tab(
-            //     child: Align(
-            //       alignment: Alignment.center,
-            //       child: Text(learn),
-            //       // \ntechnical blockchain details
-            //     ),
-            //     icon: Icon(Icons.school, color: Colors.limeAccent)),
-            // Tab(
-            //     child: Align(
-            //       alignment: Alignment.center,
-            //       child: SelectableText(danger),
-            //       //  \nexperimental software
-            //     ),
-            //     icon: Icon(Icons.dangerous, color: Colors.red)),
-          ]),
-    );
+              icon: const Icon(
+                Icons.android,
+                // color: Colors.greenAccent
+              ),
+              child: Align(
+                alignment: Alignment.center,
+                child: Text(AppLocalizations.of(context)!.danger),
+              ),
+            ),
+          ],
+        ),
+        actions: <Widget>[
+          // Icon(Icons.search),
+          PopupMenuButton<String>(
+            icon: const Icon(Icons.lunch_dining), //verticle menu
+            onSelected: onSelect,
+            color: Colors.yellowAccent,
+            itemBuilder: (BuildContext context) => [
+              PopupMenuItem(
+                  value: "Home",
+                  child: Linkify(
+                      onOpen: (link) async {
+                        if (await canLaunch(link.url)) {
+                          await launch(link.url);
+                        } else {
+                          throw 'Could not launch $link';
+                        }
+                      },
+                      // humanize: true,
+                      text: 'https://www.ethSync.org')),
+              const PopupMenuItem(
+                child: Text("beta"),
+                value: "Ethereum Website",
+              ),
+              PopupMenuItem(
+                child: Linkify(
+                    onOpen: (link) async {
+                      if (await canLaunch(link.url)) {
+                        await launch(link.url);
+                      } else {
+                        throw 'Could not launch $link';
+                      }
+                    },
+                    // humanize: true,
+                    // text: EthSyncUrl.launchURLFlutter,
+                    text: 'https://www.ethereum.org'),
+              ),
+              PopupMenuItem(
+                child: Linkify(
+                    onOpen: (link) async {
+                      if (await canLaunch(link.url)) {
+                        await launch(link.url);
+                      } else {
+                        throw 'Could not launch $link';
+                      }
+                    },
+                    // humanize: true,
+                    text: 'https://ethereum.stackexchange.com/'),
+              ),
+              const PopupMenuDivider(),
+              PopupMenuItem(
+                child: Linkify(
+                    onOpen: (link) async {
+                      if (await canLaunch(link.url)) {
+                        await launch(link.url);
+                      } else {
+                        throw 'Could not launch $link';
+                      }
+                    },
+                    text: 'https://geth.ethereum.org/'),
+              ),
+              PopupMenuItem(
+                child: Linkify(
+                    onOpen: (link) async {
+                      if (await canLaunch(link.url)) {
+                        await launch(link.url);
+                      } else {
+                        throw 'Could not launch $link';
+                      }
+                    },
+                    text: 'https://github.com/ethereum/go-ethereum'),
+              ),
+              const PopupMenuDivider(
+                height: 4.0,
+              ),
+              PopupMenuItem(
+                child: Linkify(
+                    onOpen: (link) async {
+                      if (await canLaunch(link.url)) {
+                        await launch(link.url);
+                      } else {
+                        throw 'Could not launch $link';
+                      }
+                    },
+                    text: 'https://en.wikipedia.org/wiki/Ethereum'),
+              ),
+              const PopupMenuItem(
+                child: Icon(Icons.settings, color: Colors.redAccent),
+              ),
+              const PopupMenuItem(
+                child: Text('Settings'),
+              ),
+              const PopupMenuItem(
+                child: Text('French : Francais : FR'),
+              ),
+              const PopupMenuItem(
+                child: Text('German : Deutsch : DE'),
+              ),
+              PopupMenuItem(
+                child: Linkify(
+                    onOpen: (link) async {
+                      if (await canLaunch(link.url)) {
+                        await launch(link.url);
+                      } else {
+                        throw 'Could not launch $link';
+                      }
+                    },
+                    // humanize: true,
+                    text: 'https://ethsync2022.org/'),
+              ),
+            ],
+          ),
+        ]);
   }
 }
