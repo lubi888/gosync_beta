5661b9b4b86377b661ed5e9b067d2c5b018bbe38
Linux Ubiquitous <lubi888@users.noreply.github.com>
2025-02-08 15:38:24 +0000

    cleanup home

--- lib/gosync_home.dart
+++ lib/gosync_home.dart
@@ -12,64 +12,16 @@ class GoSyncHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     () => Scaffold.of(context).openDrawer();
-    // TabController: tabController();
     debugPrint("gosync_home & tabs loaded");
     return const Scaffold(
-        // TabController tabController;
-
-        drawer: GoSyncNavDrawer(),
-        body: TabBarView(
-            // controller: TabController(length: 3, vsync: this),
-            children: <Widget>[
-              GoSyncScrollbar0(),
-              GoSyncScrollbar1(),
-              GoSyncScrollbar2(),
-              // GoSyncAbout(),
-              // gosyncscrollbar2(),
-            ])
-
-        // body: DefaultTabController(
-        //   initialIndex: 0,
-        //     length: 3,
-        //     controller: TabController(length: 3, vsync: this),
-        //     children: <Widget>[
-        //       GoSyncScrollbar0(),
-        //       GoSyncScrollbar1(),
-        //       GoSyncScrollbar2(),
-        //       // GoSyncAbout(),
-        //       // gosyncscrollbar2(),
-        //     ]
-
-        // child: Scaffold(
-        //   appBar: GoSyncAppBar(),
-        //   drawer: GoSyncNavDrawer(),
-        //   // endDrawer: GoSyncOpenDrawer(),
-        //   body: GoSyncHome(
-        //     title: goSyncTitle,
-        //   ),
-        //   //add bottom navigation from top app || side nav drawer.
-        //   // bottomNavigationBar: GoSyncBottomNavBar(),
-        //   // drawerDragStartBehavior:
-        // ),
-
-        // ),
-
-        // home: const DefaultTabController(
-        //   // controller = TabController(vsync: this, length: 3);
-        //   initialIndex: 0,
-        //   length: 3,
-        //   child: Scaffold(
-        //     appBar: GoSyncAppBar(),
-        //     drawer: GoSyncNavDrawer(),
-        //     // endDrawer: GoSyncOpenDrawer(),
-        //     body: GoSyncHome(
-        //       title: goSyncTitle,
-        //     ),
-        //     //add bottom navigation from top app || side nav drawer.
-        //     // bottomNavigationBar: GoSyncBottomNavBar(),
-        //     // drawerDragStartBehavior:
-        //   ),
-        // ),
-        );
+      drawer: GoSyncNavDrawer(),
+      body: TabBarView(
+        children: <Widget>[
+          GoSyncScrollbar0(),
+          GoSyncScrollbar1(),
+          GoSyncScrollbar2(),
+        ],
+      ),
+    );
   }
 }
